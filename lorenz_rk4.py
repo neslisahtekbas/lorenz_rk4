feda33d599f905a2a2e0ee56a4741c935aa9f1ea
neslisahtekbas <neslisahtekbas37@gmail.com>
2026-02-23 00:38:47 +0300

    lorenz_rk4.py

--- lorenz_rk4.py
+++ lorenz_rk4.py
@@ -6,7 +6,7 @@ from mpl_toolkits.mplot3d import Axes3D
 # --- 1. Parametreler ---
 x, y, z = 0.1, 1.0, 10.0
 rho, sigma, beta = 28.0, 10.0, 8.0/3.0
-t0, tf = 0, 60  # Süreyi animasyon için biraz kısalttım (çok uzun sürmesin diye)
+t0, tf = 0, 60 
 dt = 0.01       # Adım aralığı
 t = np.arange(t0, tf + dt, dt)
 n = len(t)
@@ -27,11 +27,10 @@ def RK4(t, r, f, dt):
 evol = np.zeros((n, 3))
 evol[0] = [x, y, z]
 
-# Python'da döngüleri bu şekilde optimize edebilirsin ama senin yazdığın da doğru
 for i in range(n - 1):
     evol[i + 1] = RK4(t[i], evol[i], EDOs, dt)
 
-# --- 4. Görselleştirme (Eksik Olan Kısım Burasıydı) ---
+# --- 4. Görselleştirme ---
 fig = plt.figure(figsize=(10, 10), facecolor='black') # Siyah tema
 ax = fig.add_subplot(111, projection='3d')
 ax.set_facecolor('black')
@@ -40,14 +39,14 @@ ax.set_facecolor('black')
 ax.set_xlim((-25, 25))
 ax.set_ylim((-35, 35))
 ax.set_zlim((5, 55))
-ax.grid(False) # Izgaraları kapattım daha şık dursun diye
-ax.axis('off') # Eksen çizgilerini gizle
+ax.grid(False)
+ax.axis('off') 
 
 # Çizgi ve nokta nesneleri
 line, = ax.plot([], [], [], lw=1.5, color='#FFD700')
 point, = ax.plot([], [], [], 'o', color='white')
 
-# Başlangıç fonksiyonu (Animasyon için)
+# Başlangıç fonksiyonu 
 def init():
     line.set_data([], [])
     line.set_3d_properties([])
@@ -70,17 +69,16 @@ def init():
 
 def update(frame):
     i = frame * speed_up
-    if i >= n: return line, point # Dizi sınırını aşmasın
-    
-    # Şu ana kadar olan tüm yolu çiz (iz bırakarak)
+    if i >= n: return line, point 
+
     line.set_data(evol[:i, 0], evol[:i, 1])
     line.set_3d_properties(evol[:i, 2])
     
     # En uçtaki noktayı çiz
-    point.set_data([evol[i, 0]], [evol[i, 1]]) # Liste içine almazsak hata verir
+    point.set_data([evol[i, 0]], [evol[i, 1]])
     point.set_3d_properties([evol[i, 2]])
     
-    # Kamerayı hafifçe döndür (Estetik dokunuş)
+    # Kamerayı hafifçe döndürür
     ax.view_init(elev=10, azim=i*0.25)
     
     return line, point
